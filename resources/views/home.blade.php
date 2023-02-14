3adaed891820ca7e1af1708eb398d301c5993914
Alejandro Freitas <freitassanchezalejandro@gmail.com>
2023-02-14 17:06:49 +0100

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,9 @@
 @extends('plantilla')
 
-@section('titulo', 'Listado de cortos')
+@section('titulo', 'Festicortos')
 
 @section('contenido')
 
-<ul>
-    @forelse($cortos as $corto)
-        <li>Titulo: {{ $corto['titulo'] }}</br>
-            Autor: {{$corto['autor']}}
-        </li>
-    @empty
-        <li>No hay elementos que mostrar</li>
-    @endforelse
-</ul>
+<h1>Bienvenido a festicortos</h1>
 
 @endsection
